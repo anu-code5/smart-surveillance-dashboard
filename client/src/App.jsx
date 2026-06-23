7922c2db6e01c95f33b704935cfd44188f0b0c30
Anushka <anushka.chaudhary2023@vitstudent.ac.in>
2026-06-23 23:29:39 +0530

    Add About route to the application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import About from "./pages/About"
 
 import {
  BrowserRouter,
@@ -44,6 +45,11 @@ function App() {
     <Route
       path="/dashboard"
       element={<Dashboard />}
+    />
+
+    <Route
+      path="/about"
+      element={<About />}
     />
 
    </Routes>
